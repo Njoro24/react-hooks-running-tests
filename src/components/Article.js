2f908b653c504c30300b5fec3d7276ab4ff27f38
+ <+>
2025-04-09 20:53:42 +0300

    initial changes

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,9 +3,8 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      please pass this test
     </div>
   );
 }
-
 export default Article;
